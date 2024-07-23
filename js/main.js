11ff4aa08631991615c4f715a25f855a7cab2790
HamzaJamol <hamzajamol@034gmail.com>
2024-07-23 12:06:15 +0300

    add index features

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ if (elUrlShortener) {
       // Change button bgcolor
       evt.target.classList.aff('url-shortener__copy-button--copied');
 
+      // Copy short Link to clipboard
+      console.log(evt.target.previousElementSibling.textContent);
+
       // Reset button text and class after 1 second
       setTimeout(function () {
         evt.target.textContent = 'Copy';
